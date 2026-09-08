9bb038dd5c9922cc5b981e644501d53d11ea8e5c
Tim <98615444+T-imothy@users.noreply.github.com>
2026-09-07 22:13:31 -0500

    Expose loot vote eligibility and pin Playerbots command improvements

--- src/game/Loot/LootMgr.h
+++ src/game/Loot/LootMgr.h
@@ -180,6 +180,7 @@ class GroupLootRoll
 
         bool TryToStart(Loot& loot, uint32 itemSlot);
         bool PlayerVote(Player* player, RollVote vote);
+        RollVoteMask GetVoteMaskFor(Player* player) const;
         bool UpdateRoll();
 
     private:
